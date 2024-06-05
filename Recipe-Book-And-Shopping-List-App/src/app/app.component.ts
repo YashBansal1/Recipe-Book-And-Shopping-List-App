e15c008893f8d210743bc4f1c7ead05180acd8ab
Yash Bansal <Yash.Bansal@wissen.com>
2024-06-05 10:28:23 +0530

    Added Navigation with event binding between recipe and shopping list, pass the recipe data to recipe item to display recipe list

--- Recipe-Book-And-Shopping-List-App/src/app/app.component.ts
+++ Recipe-Book-And-Shopping-List-App/src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeatures = 'recipes';
+  onNavigate(feature: string) {
+    this.loadedFeatures = feature;
+  }
+}
--- Recipe-Book-And-Shopping-List-App/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Recipe-Book-And-Shopping-List-App/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Ingredients } from '../../../shared/ingredient.model';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrl: './recipe-item.component.css'
+  styleUrl: './recipe-item.component.css',
 })
 export class RecipeItemComponent {
-
+  @Input() recipe: Recipe;
 }
